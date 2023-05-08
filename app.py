d9ed25273cf1ea868d390b2b0f592bb33449a773
Bravco <boss.wiw@gmail.com>
2023-05-08 19:54:33 +0200

    Optimizd code

--- app.py
+++ app.py
@@ -3,43 +3,47 @@ import time, random, pyaudio
 import win32gui, win32api, win32con
 
 def getInputDeviceIndex():
+    p = pyaudio.PyAudio()
     for i in range(p.get_device_count()):
         device = p.get_device_info_by_index(i)
         if device["name"].__contains__("Stereo Mix"):
             return device["index"]
 
-def cast(isBait):
-    global hwnd
+def cast():
     win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_F6, 0)
     win32api.PostMessage(hwnd, win32con.WM_KEYUP, win32con.VK_F6, 0)
-    if isBait:
-        print("BAIT")
-    else:
-        print("CATCH")
 
-hwnd = win32gui.FindWindow(None, "World of Warcraft")
-p = pyaudio.PyAudio()
+if __name__ == "__main__":
+    hwnd = win32gui.FindWindow(None, "World of Warcraft")
+    p = pyaudio.PyAudio()
 
-while True:
-    start_time = time.time()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, input_device_index = getInputDeviceIndex())
+    while True:
+        start_time = time.time()
+        stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, input_device_index = getInputDeviceIndex())
 
-    while stream.is_active():
-        data = stream.read(1024, exception_on_overflow = False)
-        data = np.frombuffer(data, dtype=np.int16) / 32768.0
-        rms = np.sqrt(np.mean(data**2))
-        db = round(20 * np.log10(rms / ((2**15) / 32768.0)))
-        print(f"dB: {db}")
-        if db > -45:
-            cast(isBait=False)
-            break
-        elif (time.time() - start_time) > 17:
-            break
+        while stream.is_active():
+            try:
+                data = stream.read(1024, exception_on_overflow = False)
+            except Exception as e:
+                print("Error reading audio stream:", e)
+                break
 
-    time.sleep(random.uniform(2, 4))
-    cast(isBait=True)
-    start_time = time.time()
-    time.sleep(random.uniform(2, 4))
+            data = np.frombuffer(data, dtype=np.int16) / 32768.0
+            rms = np.sqrt(np.mean(data**2))
+            db = round(20 * np.log10(rms / ((2**15) / 32768.0)))
+            print("dB:", db)
+            if db > -45:
+                time.sleep(random.uniform(0.75, 1.75)) # Wait before catching the fish
+                cast() # Catch
+                time.sleep(random.uniform(2, 4)) # Wait before casting a bait
+                break
+            elif (time.time() - start_time) > 17:
+                break
 
-    stream.stop_stream()
-    stream.close()
+        cast() # Bait
+        start_time = time.time()
+
+        stream.stop_stream()
+        stream.close()
+
+    p.terminate()
